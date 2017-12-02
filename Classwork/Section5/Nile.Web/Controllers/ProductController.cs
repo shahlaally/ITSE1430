f37175ea9fcc19104ab9da94c3563c6b3fbfbba2
shahla.ally <shahla.ally@NEITD-123940>
2017-12-02 12:56:02 -0600

    12/2/2017

--- Classwork/Section5/Nile.Web/Controllers/ProductController.cs
+++ Classwork/Section5/Nile.Web/Controllers/ProductController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Nile.Web.Models;
+using Nile.Stores.Sql;
 
 namespace Nile.Web.Controllers
 {
@@ -14,14 +16,52 @@ namespace Nile.Web.Controllers
 
         }
 
-        // GET: Product
-        public ActionResult List()
+        public ProductController (IProductDatabase database)
         {
-            var model = new List<ProductViewModel> {
+            _database = database;
+        }
+
+        // GET: Product
+        public ActionResult Add ()
+        {
+            var model = new List<ProductViewModel>() {
                 new ProductViewModel() {Id = 1, Name = "Product A", Price = 123}
             };
+            //var model = new ProductViewModel();
 
             return View(model);
         }
+
+        public ActionResult Delete (int id)
+        {
+            var product = _database.Get(id);
+
+            return View(product.ToModel());
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var product = _database.Get(id);
+
+            return View(product.ToModel());
+        }
+
+        public ActionResult List()
+        {
+            var products = _database.GetAll();
+
+            return View(products.ToModel());
+        }
+
+        private static  IProductDatabase GetDatabase ()
+        {
+            var connstring = ConfigurationManager.ConnectionStrings["ProductDatabase"];
+
+            return new SqlProductDatabase(connstring.ConnectionString);
+        }
+
+        
+
+    private readonly IProductDatabase  _database;
     }
 }
--- Classwork/Section5/Nile.Web/Models/ProductExtension.cs
+++ Classwork/Section5/Nile.Web/Models/ProductExtension.cs
@@ -5,8 +5,49 @@ using System.Web;
 
 namespace Nile.Web.Models
 {
-    public class ProductExtension
+    /// <summary>Provides extension methods for <see cref="Product"/>./></summary>
+    public static class ProductExtension
     {
+        /// <summary>Converts a <see cref="Product"/> to a <see cref="ProductViewModel"/>./></summary>
+        /// <param name="source" is the Product.></param>
+        /// <returns>The model.</returns>
+        public static ProductViewModel ToModel (this Product source)
+        {
+            return new ProductViewModel()
+            {
+                Id = source.Id,
+                Name = source.Name,
+                Description = source.Description,
+                Price = source.Price,
+                IsDiscontinued = source.IsDiscontinued
+            };
+        }
 
+        /// <summary>Converts a <see cref="ProductViewModel"/> to a <see cref="Product"./></summary>
+        /// <param name="source">is the ProductViewModel</param>
+        /// <returns>The Product.</returns>
+        public static Product ToDomain(this ProductViewModel source)
+        {
+            return new Product()
+            {
+                Id = source.Id,
+                Name = source.Name,
+                Description = source.Description,
+                Price = source.Price,
+                IsDiscontinued = source.IsDiscontinued
+            };
+        }
+
+        /// <summary>Converts a <see cref="Product"/> to a <see cref="ProductViewModel"/></summary>
+        /// <param name="source">The product</param>
+        /// <returns>The model</returns>
+        public static IEnumerable<ProductViewModel> ToModel(this IEnumerable<Product> source)
+        {
+            foreach (var item in source)
+                yield return item.ToModel();
+            //return from item in source
+                   //select item.ToModel();
+            
+        }
     }
 }
