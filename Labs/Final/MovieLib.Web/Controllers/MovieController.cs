81af696ef0e727d0eccc0e826a5eead7300fd865
shahla.ally <shahla.ally@NEITD-222525>
2017-12-13 18:21:36 -0600

    Final 12/13/2017

---
+++ Labs/Final/MovieLib.Web/Controllers/MovieController.cs
@@ -0,0 +1,128 @@
+﻿/*
+ * ITSE 1430
+ * Shahla Ally
+ * Final: 12/13/2017
+ */
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using MovieLib.Data.Sql;
+using MovieLib.Web.Models;
+
+namespace MovieLib.Web.Controllers
+{
+    public class MovieController : Controller
+    {
+        #region Construction
+
+        public MovieController () : this(GetDefaultStore())
+        { }
+
+        public MovieController ( IMovieDatabase database )
+        {
+            _database = database;
+        }
+        #endregion
+
+        public ActionResult Add ()
+        {
+            return View(new MovieViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Add ( MovieViewModel model )
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _database.Add(model.ToMovie());
+
+                    return RedirectToAction("List");
+                } catch (Exception e)
+                {
+                    ModelState.AddModelError("", e.Message);
+                };                
+            };
+            
+            return View(model);
+        }
+
+        public ActionResult Delete ( int id )
+        {
+            var movie = _database.Get(id);
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie.ToViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Delete ( MovieViewModel model )
+        {
+            try
+            {
+                _database.Remove(model.Id);
+                return RedirectToAction("List");
+            } catch (Exception e)
+            {
+                ModelState.AddModelError("", e.Message);
+            }
+
+            return View(model);        
+        }
+
+        public ActionResult Edit ( int id )
+        {
+            var movie = _database.Get(id);
+            if (movie == null)
+                return HttpNotFound();
+            
+            return View(movie.ToViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Edit ( MovieViewModel model )
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _database.Update(model.ToMovie());
+
+                    return RedirectToAction("List");
+                } catch (Exception e)
+                {
+                    ModelState.AddModelError("", e.Message);
+                };
+            };
+
+            return View(model);
+        }
+
+        public ActionResult List()
+        {
+            var movies = from m in _database.GetAll()
+                         orderby m.Title
+                         select m;
+            
+            return View(movies.ToViewModel());
+        }
+
+        #region Private Members
+
+        private static IMovieDatabase GetDefaultStore ()
+        {
+            var connString = ConfigurationManager.ConnectionStrings["MovieDatabase"].ConnectionString;
+
+            return new SqlMovieDatabase(connString);
+        }
+
+        private readonly IMovieDatabase _database;
+        #endregion
+    }
+}
---
+++ Labs/Final/MovieLib/ObjectValidator.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * ITSE 1430
+ * Shahla Ally
+ * Final: 12/13/2017
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLib
+{
+    /// <summary>Provides helpers for validating objects.</summary>
+    public static class ObjectValidator
+    {
+        /// <summary>Validates an object.</summary>
+        /// <param name="value">The value to validate.</param>
+        public static void ValidateObject ( IValidatableObject value )
+        {
+            Validator.ValidateObject(value, new ValidationContext(value), true);
+        }
+
+        /// <summary>Validates an object and returns the results.</summary>
+        /// <param name="value">The value.</param>
+        /// <returns>The validation results.</returns>
+        public static IEnumerable<ValidationResult> TryValidateObject ( IValidatableObject value )
+        {
+            var results = new List<ValidationResult>();
+            if (!Validator.TryValidateObject(value, new ValidationContext(value), results))
+                return results;
+
+            return Enumerable.Empty<ValidationResult>();
+        }
+    }
+}
